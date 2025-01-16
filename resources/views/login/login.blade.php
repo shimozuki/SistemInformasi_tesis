478e60f4fed46a38abaaffdcd0017add1951be3c
robbishimozuki <robbishimozuki@gmail.com>
2025-01-16 21:50:21 +0800

    update log

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -5,7 +5,7 @@
   <meta charset="utf-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SISS | Log in</title>
+  <title>SIDATA | Log in</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
--- resources/views/part/footer.blade.php
+++ resources/views/part/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="main-footer">
-  <strong>Copyright &copy; 2020 <a href="#">KKP18</a>.</strong>
+  <strong>Copyright &copy; 2025 <a href="#"></a>.</strong>
   All rights reserved.
   <div class="float-right d-none d-sm-inline-block">
-    <b>SISS Version</b> 1.0.0
+    <b>SIDATA Version</b> 1.0.0
   </div>
 </footer>
