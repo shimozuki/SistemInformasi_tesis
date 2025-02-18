d483de9312d10ac4c930ae73597b8d9d1357c42b
robbishimozuki <robbishimozuki@gmail.com>
2025-02-18 23:06:25 +0800

    master

--- resources/views/part/sidebar.blade.php
+++ resources/views/part/sidebar.blade.php
@@ -61,7 +61,7 @@
         <a href="{{url('/pascasidang')}}" class="nav-link">
           <i class="nav-icon fas fa-file-upload"></i>
           <p>
-            Pascasidang
+            Upload Berkas
           </p>
         </a>
       </li>
