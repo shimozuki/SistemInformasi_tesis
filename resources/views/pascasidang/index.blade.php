5df1e42984986c886b35c4b58847a6beb8d11cd1
robbishimozuki <robbishimozuki@gmail.com>
2025-02-18 22:34:15 +0800

    sip

--- resources/views/pascasidang/index.blade.php
+++ resources/views/pascasidang/index.blade.php
@@ -9,12 +9,12 @@
     <div class="container-fluid">
         <div class="row mb-2">
             <div class="col-sm-6">
-                <h1>Data Pascasidang</h1>
+                <h1>Data Pengumpulan Berkas</h1>
             </div>
             <div class="col-sm-6">
                 <ol class="breadcrumb float-sm-right">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Data Pascasidang</li>
+                    <li class="breadcrumb-item active">Data Pengumpulan Berkas</li>
                 </ol>
             </div>
         </div>
