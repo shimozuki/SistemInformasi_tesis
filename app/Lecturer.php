ce20db12b6f1689851df2e77f6917ab84b20312c
robbishimozuki <robbishimozuki@gmail.com>
2025-03-19 15:16:40 +0800

    add

--- app/Lecturer.php
+++ app/Lecturer.php
@@ -8,4 +8,17 @@ class Lecturer extends Model
 {
     protected $primaryKey = 'nidn';
     public $incrementing = false;
+    protected $fillable = [
+        'nidn',
+        'tahun',
+        'id_auth',
+        'id_grup',
+        'id_jadwal',
+        'nama',
+        'alamat',
+        'no_hp',
+        'email',
+        'pembimbing',
+        'penguji'
+    ];
 }
